800234e4bf2f78b1638f3844e7f6ee19fc64d9bc
drondu <drondu98@gmail.com>
2021-05-07 14:58:30 +0300

    working with DBs

--- start.sh
+++ start.sh
@@ -3,8 +3,16 @@
 
 echo "Starting scripts"
 
-
-hostname > hostname.txt
+killall diskUsage.sh python3 ifstat network.sh
+sudo ls
+cd diskUsage/
+rm -rf diskTempStripped.txt diskTemp.txt diskUsage.db diskUsageStripped.txt diskUsage.txt
+cd ..
+cd networkUsage
+rm -rf netUsage.db net.log
+cd ..
+rm -rf hostname.txt 
+sudo hostname > hostname.txt
 cd diskUsage && nohup ./diskUsage.sh </dev/null >/dev/null 2>&1 &
 cd networkUsage && nohup ./network.sh </dev/null >/dev/null 2>&1 &
 
