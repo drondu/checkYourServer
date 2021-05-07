e609dbbfcf958237b53d6313dd5708e2b10063ed
drondu <drondu98@gmail.com>
2021-05-07 18:41:10 +0300

    something is working with aggregated servers

---
+++ destroy.sh
@@ -0,0 +1,8 @@
+killall start.sh
+killall nano
+killall diskUsage.sh
+killall ifstat
+killall python3
+killall network.sh
+killall sleep
+killall transferDB.sh
--- transferDB.sh
+++ transferDB.sh
@@ -5,7 +5,8 @@ while :
 do
 	sleep 5
 
-	scp diskUsage/diskUsage.db drondu@10.124.191.1:~/Desktop/licenta/checkYourServer/DBs/diskUsageServer0.db
-	scp networkUsage/netUsage.db drondu@10.124.191.1:~/Desktop/licenta/checkYourServer/DBs/netUsageServer0.db
+	sshpass -p7022 scp diskUsage/diskUsage.db drondu@10.124.191.1:~/Desktop/licenta/checkYourServer/DBs/diskUsageServer0.db
+	sshpass -p7022 scp networkUsage/netUsage.db drondu@10.124.191.1:~/Desktop/licenta/checkYourServer/DBs/netUsageServer0.db
 
+	rm -rf diskUsage/diskUsage.db networkUsage/netUsage.db
 done
