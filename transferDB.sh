a4e0e480dbf6803dd2fe8f81eb5a56d5df0f59bf
drondu <drondu98@gmail.com>
2021-05-11 09:58:28 +0300

    yes

--- transferDB.sh
+++ transferDB.sh
@@ -5,7 +5,7 @@ value=$(<hostname.txt)
 
 while :
 do
-	sleep 5
+	sleep 3
 
 	sshpass -p7022 scp diskUsage/diskUsage.db daniel@10.238.184.1:~/Desktop/checkYourServer/DBs/diskUsage$value.db
 	sshpass -p7022 scp networkUsage/netUsage.db daniel@10.238.184.1:~/Desktop/checkYourServer/DBs/netUsage$value.db
