f895e9de91a0fd00bd3779fa6c860b46825ce606
drondu <drondu98@gmail.com>
2021-06-13 12:01:34 +0300

    pyhelpers

--- networkUsage/networkStats.py
+++ networkUsage/networkStats.py
@@ -1,9 +1,11 @@
 #! /usr/bin/p9ython
-from datetime import datetime
 import time
-import string
-import random 
-from random import randint
+import sys
+sys.path.append('../pyHelpers')
+
+from gethost import getUserName
+import timeandid as tid
+import time
 
 fin = open('net.log','r')
 
@@ -14,70 +16,58 @@ def readNames(names):
 	while line and lineCount < 2:
 		time.sleep(1)
 		for el in line.split():
-			if lineCount == 0:
+			if lineCount == 0: 
 				names.append(el)
 				continue
 			if lineCount == 1:
 				break
-
+			
 		lineCount += 1
 		line = fin.readline()
-
-def getHostName():
-	hin = open('../hostname.txt', 'r')
-	name = hin.readline()
-	hin.close()
-	return name.rstrip('\n')
-
 
 def readValues():
 	lineCount = 0
 	names = []
+	# print("hope")
 	readNames(names)
 	line = fin.readline()
 	while line:
+		# print("john")
+		fout = open('../DBs/netUsageMother.db', 'a')
 		time.sleep(1)
 		columnCnt = 0
 		namesCnt = 0
-		fout = open('netUsage.db', 'a')
 		for el in line.split():
 			if namesCnt >= len(names):
 				namesCnt = 0
 			elif columnCnt >= 2:
 				columnCnt = 0
 			if columnCnt == 0:
-				temp = '{"host":"' + getHostName()
-				temp += '","name":"' + names[namesCnt] 
+				temp = getUserName()
+				temp += '","net_driver":"' + names[namesCnt] 
 				temp += '","dwnSpeed":"' + el
 			elif columnCnt == 1:
 				temp += '","upSpeed":"' + el
-				temp += getIdAndTimeStamp()	
-				#print(temp)
+				temp += tid.getTimeStamp() + tid.getID()	
+				print(temp)
 				fout.write(temp)
 				fout.flush()
 				temp = ''
 			columnCnt += 1
 			if columnCnt == 2:
 				namesCnt += 1
+		fout.close()
 		lineCount += 1
 
 		if lineCount == 21:
 			lineCount = 0	
 		if lineCount == 0:
-			readNames(names)			 
+			readNames(names)
+			 
 		line = fin.readline()
 
-def getIdAndTimeStamp():
-	ts = int(int(datetime.now().strftime("%s%f"))/1000)
-	chars = string.ascii_letters + string.digits
-	temp = '","timestamp":' + str(ts)
-	temp += ',"_id":"'+ ''.join(random.choices(chars, k=30)) 
-	temp +=  '"}\n'
-	return temp
 
-
-getHostName()
+#readNames()
 readValues()
 
 fin.close()
-fout.close()
---
+++ pyHelpers/diskspace.py
@@ -0,0 +1,32 @@
+import time
+
+def getDiskSpace(fin):
+	count = 0
+	line = fin.readline()
+	for el in line.split():
+		time.sleep(0.1)
+		if count == 0:
+			name = el
+		if count == 2:
+			el.rstrip()
+			temp = ''
+			for nb in el:
+				if nb.isdigit():
+					temp += nb		
+			used = temp
+		if count == 3:
+			el.rstrip()
+			temp = ''
+			for nb in el:
+				if nb.isdigit():
+					temp += nb
+			available = temp
+			
+		count+=1
+
+
+	temp = '","diskname":"' + name 
+	temp += '","used":"' + used 
+	temp += '","available":"' + available
+
+	return temp 
